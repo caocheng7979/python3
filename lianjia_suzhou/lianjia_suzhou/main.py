43731c31c86740ca9efd73109ad63f026c0c31cb
caocheng <caocheng7979@gmail.com>
2017-11-22 23:49:34 +0900

    new

---
+++ lianjia_suzhou/lianjia_suzhou/main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python  
+# -*- encoding:utf-8 -*-  
+#====#====#====#==== 
+# __author__ = "Cheng Cao"   
+#FileName: *.py  
+#Version:1.0.0  
+#====#====#====#==== 
+
+from scrapy import  cmdline
+cmdline.execute(['Scrapy','crawl','lianjia_spider'])
+#'Scrapy crawl lianjia_spider'.split()
+
